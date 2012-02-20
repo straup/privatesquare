52164892b8a7ae5b74927de14af4817537732ecf
straup <straup@gmail.com>
2012-02-19 22:29:21 -0500

    starting using lib_urls; add lib_foursquare_urls

--- www/include/lib_urls.php
+++ www/include/lib_urls.php
@@ -1,5 +1,32 @@
 <?php
 
-	# Please, write me...
+	loadlib("foursquare_users");
 
+ 	#################################################################
+
+	function urls_venue(&$venue){
+
+		return $GLOBALS['cfg']['abs_root_url'] . "venue/{$venue['venue_id']}/";
+	}
+
+ 	#################################################################
+
+	function urls_history_for_user(&$user){
+
+		$fsq_user = foursquare_users_get_by_user_id($user['id']);
+
+		return $GLOBALS['cfg']['abs_root_url'] . "user/{$fsq_user['foursquare_id']}/history/";
+	}
+
+ 	#################################################################
+
+	function urls_checkin(&$checkin){
+
+		$user = users_get_by_id($checkin['user_id']);
+		$fsq_user = foursquare_users_get_by_user_id($user['id']);
+
+		return $GLOBALS['cfg']['abs_root_url'] . "user/{$fsq_user['foursquare_id']}/checkin/{$checkin['id']}/";
+	}
+
+ 	#################################################################
 ?>
