8ecdeb74e34cf4b622992457c8eec2483851c47b
straup <straup@gmail.com>
2012-02-26 16:11:29 -0500

    clean up fh handling; add export_send

--- www/include/lib_export.php
+++ www/include/lib_export.php
@@ -2,18 +2,26 @@
 
 	##############################################################################
 
-	function export_default_filehandle(){
+	function export_send(&$fh, $more=array()){
 
-		$fh = fopen("php://stdout", "w");
-		return $fh;
+		$defaults = array(
+
+		);
+
+		$more = array_merge($defaults, $more);
+
+		rewind($fh);
+
+		echo stream_get_contents($fh);
 	}
 
 	##############################################################################
 
 	function export_massage_checkin(&$row){
-		# note the pass-by-ref
 
 		unset($row['venue']);
+
+		# note the pass-by-ref
 	}
 
 	##############################################################################
