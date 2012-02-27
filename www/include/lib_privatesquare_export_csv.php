6a8d4e847a1b93d6e816187cf60079fc4c921fbd
straup <straup@gmail.com>
2012-02-26 20:25:56 -0500

    proper filename; make it work

---
+++ www/include/lib_privatesquare_export_csv.php
@@ -0,0 +1,31 @@
+<?php
+
+	loadlib("export");
+
+	##############################################################################
+
+	function privatesquare_export_csv($fh, $checkins, $more=array()){
+
+		fputcsv($fh, array_keys($checkins[0]));
+
+		foreach ($checkins as $row){
+			privatesquare_export_massage_checkin($row);
+			fputcsv($fh, array_values($row));
+		}
+
+		if (isset($more['donot_send'])){
+			return okay();
+		}
+
+		$map = privatesquare_export_valid_formats();
+
+		$headers = array(
+			'Content-type' => $map['csv'],
+		);
+
+		privatesquare_export_send($fh, $headers, $more);
+	}
+
+	##############################################################################
+
+?>
