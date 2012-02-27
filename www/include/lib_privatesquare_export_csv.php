698815bf326608385a8328d0557debd5a3e829de
straup <straup@gmail.com>
2012-02-27 15:02:13 -0500

    notes inre: field names; sad face

--- www/include/lib_privatesquare_export_csv.php
+++ www/include/lib_privatesquare_export_csv.php
@@ -6,6 +6,13 @@
 
 	function privatesquare_export_csv($fh, $checkins, $more=array()){
 
+		# TO DO: this works fine until we want to "explode" specific
+		# fields (like 'weather') because we need to find all the distinct
+		# keys that might be used (across both fields and potential
+		# providers: google weather versus some other api). This is
+		# either a head scratch or a pain in the ass or both...
+		# (2012027/straup)
+
 		fputcsv($fh, array_keys($checkins[0]));
 
 		foreach ($checkins as $row){
