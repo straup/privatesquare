7d76852b7767e8a0295eed767930507fb4a23deb
straup <straup@gmail.com>
2012-02-29 19:58:40 -0500

    inittial commit

---
+++ www/user_place.php
@@ -0,0 +1,49 @@
+<?php
+
+	include("include/init.php");
+
+	loadlib("privatesquare_checkins");
+	loadlib("foursquare_users");
+
+	$fsq_id = get_int32("foursquare_id");
+
+	if (! $fsq_id){
+		error_404();
+	}
+
+	$woeid = get_int32("woeid");
+
+	if (! $woeid){
+		error_404();
+	}
+
+	$history_url = "user/{$fsq_id}/places/{$woeid}/";
+	login_ensure_loggedin($history_url);
+
+	$fsq_user = foursquare_users_get_by_foursquare_id($fsq_id);
+
+	if (! $fsq_user){
+		error_404();
+	}
+
+	$owner = users_get_by_id($fsq_user['user_id']);
+	$is_own = ($owner['id'] == $GLOBALS['cfg']['user']['id']) ? 1 : 0;
+
+	# for now...
+
+	if (! $is_own){
+		error_403();
+	}
+
+	$more = array(
+		'locality' => $woeid,
+	);
+
+	$rsp = privatesquare_checkins_venues_for_user($owner, $more);
+	$GLOBALS['smarty']->assign_by_ref("venues", $rsp['rows']);
+
+	$GLOBALS['smarty']->assign_by_ref("owner", $owner);
+
+	$GLOBALS['smarty']->display("page_user_place.txt");
+	exit();
+?>
