c50a29f7373dc2eaafbf0e3d28d9527348357613
straup <straup@gmail.com>
2012-03-07 00:55:32 -0500

    remove all the javascript AND RETURN GIBBERISH

--- www/include/lib_suncalc.php
+++ www/include/lib_suncalc.php
@@ -16,100 +16,138 @@
 
 	# constants for sun calculations
 
-	define(SUNCALC_RAD, M_PI / 180);
-
-	define(SUNCALC_DAYSMS, (1000 * 60 * 60 * 24));
-	define(SUNCALC_J1970, 2440588);
-	define(SUNCALC_J2000, 2451545);
-	define(SUNCALC_M0, SUNCALC_RAD * 357.5291);
-	define(SUNCALC_M1, SUNCALC_RAD * 0.98560028);
-	define(SUNCALC_J0, 0.0009);
-	define(SUNCALC_J1, 0.0053);
-	define(SUNCALC_J2, -0.0069);
-	define(SUNCALC_C1, SUNCALC_RAD * 1.9148);
-	define(SUNCALC_C2, SUNCALC_RAD * 0.0200);
-	define(SUNCALC_C3, SUNCALC_RAD * 0.0003);
-	define(SUNCALC_P, SUNCALC_RAD * 102.9372);
-	define(SUNCALC_e, SUNCALC_RAD * 23.45);
-	define(SUNCALC_th0, SUNCALC_RAD * 280.1600);
-	define(SUNCALC_th1, SUNCALC_RAD * 360.9856235);
+	define('SUNCALC_RAD', M_PI / 180);
+	define('SUNCALC_DAYSMS', (1000 * 60 * 60 * 24));
+	define('SUNCALC_J1970', 2440588);
+	define('SUNCALC_J2000', 2451545);
+	define('SUNCALC_M0', SUNCALC_RAD * 357.5291);
+	define('SUNCALC_M1', SUNCALC_RAD * 0.98560028);
+	define('SUNCALC_J0', 0.0009);
+	define('SUNCALC_J1', 0.0053);
+	define('SUNCALC_J2', -0.0069);
+	define('SUNCALC_C1', SUNCALC_RAD * 1.9148);
+	define('SUNCALC_C2', SUNCALC_RAD * 0.0200);
+	define('SUNCALC_C3', SUNCALC_RAD * 0.0003);
+	define('SUNCALC_P', SUNCALC_RAD * 102.9372);
+	define('SUNCALC_e', SUNCALC_RAD * 23.45);
+	define('SUNCALC_th0', SUNCALC_RAD * 280.1600);
+	define('SUNCALC_th1', SUNCALC_RAD * 360.9856235);
 
 	# date conversions
 
-	function suncalc_dateToJulianDate($date){
-		# FIX ME
-		return date.valueOf() / SUNCALC_DAYSMS - 0.5 + SUNCALC_J1970;
+ 	#################################################################
+
+	function suncalc_date_to_julian_date($date){
+		$ms = strtotime($date) * 1000;
+		return $ms / SUNCALC_DAYSMS - 0.5 + SUNCALC_J1970;
 	}
 
-	function suncalc_julianDateToDate($j){
-		# FIX ME
-		return new Date((j + 0.5 - SUNCALC_J1970) * SUNCALC_DAYSMS);
+ 	#################################################################
+
+	function suncalc_julian_date_to_date($j){
+		$ms = ($j + 0.5 - SUNCALC_J1970) * SUNCALC_DAYSMS;
+		$ts = $ms / 1000;
+		return gmdate('c', $ts);
 	}
 
 	# general sun calculations
+
+ 	#################################################################
 
 	function suncalc_get_julian_cycle($J, $lw){
 		return round($J - SUNCALC_J2000 - SUNCALC_J0 - $lw / (2 * M_PI));
 	}
 
+ 	#################################################################
+
 	function suncalc_get_solar_mean_anomaly($Js){
 		return SUNCALC_M0 + SUNCALC_M1 * ($Js - SUNCALC_J2000);
 	}
+
+ 	#################################################################
 
 	function suncalc_get_equation_of_center($M){
 		return SUNCALC_C1 * sin($M) + SUNCALC_C2 * sin(2 * $M) + SUNCALC_C3 * sin(3 * $M);
 	}
 
+ 	#################################################################
+
 	function suncalc_get_ecliptic_longitude($M, $C){
 		return $M + SUNCALC_P + $C + M_PI;
 	}
 
+ 	#################################################################
+
 	function suncalc_get_sun_declination($Ls){
-		return asin(sin($Ls) * sin(SUNCALC_E));
+		return asin(sin($Ls) * sin(SUNCALC_e));
 	}
 
+ 	#################################################################
+
 	# calculations for sun times
+
+ 	#################################################################
 
 	function suncalc_get_approx_transit($Ht, $lw, $n){
 		return SUNCALC_J2000 + SUNCALC_J0 + ($Ht + $lw) / (2 * M_PI) + $n;
 	}
 
+ 	#################################################################
+
 	function suncalc_get_solar_transit($Js, $M, $Ls){
 		return $Js + (SUNCALC_J1 * sin($M)) + (SUNCALC_J2 * sin(2 * $Ls));
 	}
+
+ 	#################################################################
 
 	function suncalc_get_hour_angle($h, $phi, $d){
 		return acos((sin($h) - sin($phi) * sin($d)) / (cos($phi) * cos($d)));
 	}
 
+ 	#################################################################
+
 	# calculations for sun position
+
+ 	#################################################################
 
 	function suncalc_get_right_ascension($Ls){
 		return atan2(sin($Ls) * cos(SUNCALC_e), cos($Ls));
 	}
 
+ 	#################################################################
+
 	function suncalc_get_sidereal_time($J, $lw){
 		return SUNCALC_th0 + SUNCALC_th1 * ($J - SUNCALC_J2000) - $lw;
 	}
+
+ 	#################################################################
 
 	function suncalc_get_azimuth($H, $phi, $d) {
 		return atan2(sin($H), cos($H) * sin($phi) - tan($d) * cos($phi));
 	}
 
+ 	#################################################################
+
 	function suncalc_get_altitude($H, $phi, $d){
 		return asin(sin($phi) * sin($d) + cos($phi) * cos($d) * cos($H));
 	}
+
+ 	#################################################################
 
 	function suncalc_add_time($angle, $rise_name, $set_name){
 		$GLOBALS['suncalc_times'][] = array($angle, $rise_name, $set_name);
 	}
 
+ 	#################################################################
+
 	# calculates sun times for a given date and latitude/longitude
+
+ 	#################################################################
 
 	function suncalc_get_times($date, $lat, $lon){
 
-		$lw = SUNCALC_RAD * -lng;
-		$phi = SUNCALC_RAD * lat;
+		$lw = SUNCALC_RAD * -$lon;
+		$phi = SUNCALC_RAD * $lat;
 
 		$J = suncalc_date_to_julian_date($date);
 		$n = suncalc_get_julian_cycle($J, $lw);
@@ -148,9 +186,10 @@
 			$result[$eveningName] = suncalc_julian_date_to_date($Jset);
 		}
 
-		return result;
-	};
+		return $result;
+	}
 
+ 	#################################################################
 
 	# calculates sun azimuth and altitude for a given date and latitude/longitude
 
@@ -176,4 +215,5 @@
 		);
 	}
 
+ 	#################################################################
 ?>
