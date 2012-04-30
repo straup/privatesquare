0119fa194813a066eb58276f888eb64fd67cdb63
straup <straup@gmail.com>
2012-04-29 21:44:05 -0400

    various bug fixes; wrangling

--- www/javascript/privatesquare.deferred.js
+++ www/javascript/privatesquare.deferred.js
@@ -2,11 +2,11 @@ function privatesquare_deferred_checkin(lat, lon, reason){
 
 	 privatesquare_unset_status();
 
-	if (reason === 'offline'){
+	if (reason=='offline'){
 		privatesquare_set_status("I've lost the network.");
 	}
 
-	if (reason === 'api error'){
+	else if (reason=='api error'){
 		privatesquare_set_status("foursquare is sad.");
 	}
 
@@ -14,14 +14,17 @@ function privatesquare_deferred_checkin(lat, lon, reason){
 		privatesquare_set_status(reason);
 	}
 
+	else {}
+
 	_privatesquare_show_map(lat, lon);
 
 	var w = $("#deferred_where");
+
 	w.attr("data-latitude", lat);
 	w.attr("data-longitude", lon);
 
 	$("#deferred_checkin").submit(privatesquare_deferred_checkin_submit);
-	$("#deferred_checkin").show();
+	$("#deferred").show();
 }
 
 function privatesquare_deferred_checkin_submit(){
@@ -32,14 +35,23 @@ function privatesquare_deferred_checkin_submit(){
 	var w = $("#deferred_where");
 
 	var checkin = {
-		'location': w.val(),
+		'venue': w.val(),
 		'latitude': w.attr('data-latitude'),
 		'longitude': w.attr('data-longitude'),
 		'created': ts
 	};
 
-	privatesquare_deferred_store(checkin, function(){ alert("STORED"); });
+	privatesquare_deferred_store(checkin, privatesquare_deferred_checkin_stored);
 	return false;
+}
+
+function privatesquare_deferred_checkin_stored(checkin){
+
+	var msg = "Okay! Your checkin at " + htmlspecialchars(checkin['venue']) + " has been recorded.";
+	privatesquare_set_status(msg);
+
+	_privatesquare_hide_map();
+	$("#deferred_checkin").hide();
 }
 
 function privatesquare_deferred_indicator(){
@@ -49,8 +61,8 @@ function privatesquare_deferred_indicator(){
 
 	var count = (deferred) ? deferred.length : 0;
 
-	var wrapper = $("#deferred");
-	var indicator = $("#deferred_count");
+	var wrapper = $("#pending");
+	var indicator = $("#pending_count");
 
 	if (count){
 		indicator.html(count + " pending");
@@ -80,4 +92,9 @@ function privatesquare_deferred_store(checkin, callback){
 	if (callback){
 		callback(checkin);
 	}
+}
+
+function privatesquare_deferred_purge(){
+	var store = new Store("privatesquare");
+	store.remove("deferred");
 }
