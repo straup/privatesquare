380ff0d5f40d52d61b4454ec9b8d3110d7e4562a
straup <straup@gmail.com>
2012-07-31 06:45:23 -0400

    there is no desire in the atlas of meh

--- www/test.php
+++ www/test.php
@@ -1,7 +1,22 @@
 <?php
 
 	include("include/init.php");
+	loadlib("brooklyn_integers_api");
+	loadlib("mission_integers_api");
 
-	dumper(crumb_generate("api", "test.echo"));
+	exit();
+
+	$method = "next-int";
+
+	# $rsp = mission_integers_api_call($method);
+	# dumper($rsp);
+	# exit();
+
+	$method = "brooklyn.integers.create";
+	$args = array('count' => 2);
+
+	$rsp = brooklyn_integers_api_post($method, $args);
+	dumper($rsp);
+
 	exit();
 ?>
