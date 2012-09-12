b7595bf87b2204ce13e1482b9310871c79f63cd2
Nolan Caudill <nolan@nolancaudill.com>
2012-09-12 14:42:50 -0700

    Got'em, let's save'em

--- www/include/lib_weather_yahoo.php
+++ www/include/lib_weather_yahoo.php
@@ -23,17 +23,23 @@
 		$doc = new SimpleXMLElement($rsp['body']);
 		$doc->registerXPathNamespace('yweather', 'http://xml.weather.yahoo.com/ns/rss/1.0');
 		$conditions = $doc->xpath('//yweather:condition');
+		$wind_conds = $doc->xpath('//yweather:wind');
+		$atmosphere_conds = $doc->xpath('//yweather:atmosphere');
 
 		if (! $conditions){
 			return not_okay("failed to parse conditions");
 		}
 
 		$condition_attrs = $conditions[0]->attributes();
+		$wind_attrs = $wind_conds[0]->attributes();
+		$atmosphere_attrs = $atmosphere_conds[0]->attributes();
 
 		$normalized_conds = array();
 		$normalized_conds['condition'] = (string)$condition_attrs['text'];
 		$normalized_conds['temp_f'] = (string)$condition_attrs['temp'];
 		$normalized_conds['temp_c'] = round(($normalized_conds['temp_f'] - 32) * (5/9));
+		$normalized_conds['wind'] = (string)$wind_attrs['speed'] . " mph";
+		$normalized_conds['humidity'] = (string)$atmosphere_attrs['humidity'] . '%';
 
 		$rsp = array(
 			'latitude' => $lat,
