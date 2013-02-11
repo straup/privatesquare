5b1bda7caf6e49a795db11d94414a22fd22f8d33
straup <straup@gmail.com>
2013-02-11 08:55:15 -0500

    basic 4sq venue URL parsing

--- www/venue_i_want_to_go_there.php
+++ www/venue_i_want_to_go_there.php
@@ -10,6 +10,21 @@
 
 		$GLOBALS['smarty']->assign("step", "confirm");
 
+		# Mostly because I am not sure what the check for when a 4sq URL
+		# has a clean name in it, like this:
+		# https://foursquare.com/v/yakitori-totto/454b1ad3f964a520a73c1fe3
+
+		if (preg_match("/https?:\/\/foursquare.com\/v\/.*/", $venue_id)){
+
+			if (preg_match("/\/([a-fA-F0-9]+)\/?$/", $venue_id, $m)){
+				$venue_id = $m[1];
+			}
+
+			if (! $venue_id){
+
+			}
+		}
+
 		# check if venue_id is a URL and strip
 
 		$venue = foursquare_venues_get_by_venue_id($venue_id);
