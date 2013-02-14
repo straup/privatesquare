683061b47cee162ed1e27ad7fabbaf16b15173e9
straup <straup@gmail.com>
2013-02-14 17:46:16 -0500

    bolt in _tile_url function; still need to add MM

--- www/include/lib_littleprinter.php
+++ www/include/lib_littleprinter.php
@@ -7,15 +7,16 @@
 
 	function littleprinter_print_venue(&$venue, &$user){
 
+		$berg_user = bergcloud_users_get_by_user_id($user['id']);
+
 		# TO DO: error handling...
 
 		$venue['data'] = json_decode($venue['data'], 'as hash');
-
-		# TO DO: lat,lon (and zoom) to tile
-
-		$berg_user = bergcloud_users_get_by_user_id($user['id']);
-
 		$GLOBALS['smarty']->assign_by_ref("venue", $venue);
+
+		$tile_url = littleprinter_tile_url($venue['latitude'], $venue['longitude']);
+		$GLOBALS['smarty']->assign("tile_url", $tile_url);
+
 		$msg = $GLOBALS['smarty']->fetch("page_littleprinter_venue.txt");
 
 		$code = $berg_user['direct_print_code'];
@@ -24,4 +25,22 @@
 		return $rsp;
 	}
 
-	########################################################################	
+	########################################################################
+
+	function littleprinter_tile_url($lat, $lon, $zoom=15){
+
+		$provider = new MMaps_OpenStreetMap_Provider();
+
+		$pt = new MMaps_Location($lat, $lon);
+
+		$coord = $provider->locationCoordinate($pt);
+		$coord = $coord->zoomTo($zoom);
+
+		$x = intval($coord->column);
+		$y = intval($coord->row);
+		$z = intval($coord->zoom);
+
+		return "http://tile.stamen.com/toner/{$z}/{$x}/{$y}.jpg";
+	}
+
+	########################################################################
