0e47d93401db92bdd619204aa16ed18911d35af0
straup <straup@gmail.com>
2013-02-13 22:03:55 -0500

    stub out actual sending of messages to LP

---
+++ www/include/lib_littleprinter.php
@@ -0,0 +1,25 @@
+<?php
+
+	loadlib("bergcloud_users");
+	loadlib("bergcloud_littleprinter");
+
+	########################################################################
+
+	function littleprinter_print_venue(&$venue, &$user){
+
+		$venue['data'] = json_decode($venue['data'], 'as hash');
+
+		# TO DO: error handling...
+
+		$berg_user = bergcloud_users_get_by_user_id($user['id']);
+
+		$GLOBALS['smarty']->assign_by_ref("venue", $venue);
+		$msg = $GLOBALS['smarty']->fetch("page_littleprinter_venue.txt");
+
+		$code = $berg_user['direct_print_code'];
+
+		$rsp = bergcloud_littleprinter_direct_print($msg, $code);
+		return $rsp;
+	}
+
+	########################################################################	
