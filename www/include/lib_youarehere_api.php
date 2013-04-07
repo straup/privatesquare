0f6005624bba658d3929a51697e40656cf8a045c
straup <straup@gmail.com>
2013-04-07 12:20:12 -0400

    chip away at youarehere oauth stuff...

--- www/include/lib_youarehere_api.php
+++ www/include/lib_youarehere_api.php
@@ -2,20 +2,50 @@
 
 	loadlib("http");
 
-	$GLOBALS['youarehere_api_endpoint'] = '';
+	$GLOBALS['youarehere_host'] = '';
+	$GLOBALS['youarehere_api_endpoint'] = 'api/rest/';
 	$GLOBALS['youarehere_auth_endpoint'] = '';
 
 	########################################################################
 
-	function youarehere_api_auth_user(){
+	function youarehere_api_auth_user_url(){
 
+		$args = array(
+			'scope' => '',
+			'api_key' => '',
+			'redirect_uri' => $GLOBALS['cfg']['abs_root_url'] . 'youarehere/auth/',
+		);
+
+		$query = http_build_query($args);
+
+		$url = $GLOBALS['youarehere_host'] . $GLOBALS['youarehere_auth_endpoint'] . '?'. $query;
+		return $url;
 	}
 
 	########################################################################
 
-	function youarehere_api_call($method, $args=array()){
+	function youarehere_api_call($method, $args=array(), $more=array()){
 
+		$args['method'] = $method;
 
+		$url = $GLOBALS['youarehere_host'] . $GLOBALS['youarehere_api_endpoint'];
+
+		$rsp = http_post($url, $args);
+
+		if (! $rsp['ok']){
+			return $rsp;
+		}
+
+		$data = json_decode($rsp['body'], 'as hash');
+
+		if (! $data){
+			$rsp['ok'] = 0;
+			$rsp['error'] = 'Failed to parse JSON';
+			return $rsp;
+		}
+
+		$rsp['data'] = $data;
+		return $rsp;
 	}
 
 	########################################################################
