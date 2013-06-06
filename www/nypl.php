12522f7692716201302f9828d4cce49ca7aea889
straup <straup@gmail.com>
2013-06-06 18:37:39 -0400

    remove cooperhewitt stuff; disable youarehere by default; gosh this branch got confused

---
+++ www/nypl.php
@@ -0,0 +1,23 @@
+<?php
+
+	include("include/init.php");
+
+	if (! $GLOBALS['cfg']['user']['id']){
+
+		header("location: /");
+		exit();
+	}
+
+	loadlib("privatesquare_checkins");
+	loadlib("foursquare_checkins");
+
+	$status_map = privatesquare_checkins_status_map();
+	$broadcast_map = foursquare_checkins_broadcast_map();
+
+	$GLOBALS['smarty']->assign_by_ref("status_map", $status_map);
+	$GLOBALS['smarty']->assign_by_ref("broadcast_map", $broadcast_map);
+
+	$GLOBALS['smarty']->display("page_nypl.txt");
+	exit();
+
+?>
