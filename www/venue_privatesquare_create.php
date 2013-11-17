245cfc5765fa9c702549977b37e4467d053d85d8
straup <straup@gmail.com>
2013-11-17 18:09:48 -0500

    create place form

---
+++ www/venue_privatesquare_create.php
@@ -0,0 +1,12 @@
+<?php
+
+	include("include/init.php");
+
+	login_ensure_loggedin();
+
+	$create_crumb = crumb_generate("api", "privatesquare.venues.create");	
+	$GLOBALS['smarty']->assign("create_crumb", $create_crumb);
+
+	$GLOBALS['smarty']->display("page_venue_privatesquare_create.txt");
+	exit();
+?>
