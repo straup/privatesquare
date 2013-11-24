cd6ccd49450370b2d76b032ff3e8723c94f4a61f
straup <straup@gmail.com>
2013-11-24 17:03:16 -0500

    first batch of changes to get things working (post migration)

--- bin/backfill_migrate_foursquare_venues.php
+++ bin/backfill_migrate_foursquare_venues.php
@@ -11,9 +11,20 @@
 
 	# THIS HAS NOT BEEN TESTED YET (20131124/straup)
 
+	$GLOBALS['start'] = 0;
+
 	function migrate_venue($row){
 
 		$venue = $row;
+
+		if ($venue['venue_id'] == '4c5fb27847429c74b1991a7a'){
+			$GLOBALS['start'] = 1;
+			return;
+		}
+
+		if (! $GLOBALS['start']){
+			return;
+		}
 
 		$venue['provider_id'] = venues_providers_label_to_id("foursquare");
 		$venue['provider_venue_id'] = $row['venue_id'];
@@ -21,9 +32,6 @@
 		if ((isset($venue['latitude'])) && (isset($venue['longitude']))){
 			venues_geo_append_hierarchy($venue['latitude'], $venue['longitude'], $venue);
 		}
-
-		dumper($venue);
-		return;
 
 		$rsp = venues_add_venue($venue);
 
@@ -35,19 +43,25 @@
 		$venue_id = $rsp['venue']['venue_id'];
 		$foursquare_id = $rsp['venue']['provider_venue_id'];
 
+		echo "{$foursquare_id} : {$venue_id}\n";
+		return;
+
 		$enc_venue_id = AddSlashes($venue_id);
 		$enc_foursquare_id = AddSlashes($foursquare_id);
 
 		$sql = "UPDATE PrivatesquareCheckins SET venue_id='{$enc_venue_id}' WHERE venue_id='{$enc_foursquare_id}'";
 
 		foreach ($GLOBALS['cfg']['db_users']['host'] as $cluster_id => $ignore){
-			db_write_users($cluster_id, $sql);
+			dumper($sql);
+			$rsp = db_write_users($cluster_id, $sql);
 		}
 
 	}
 
+	$more = array('per_page' => 100);
+
 	$sql = "SELECT * FROM FoursquareVenues";
-	backfill_db_users($sql, "migrate_venue");
+	backfill_db_users($sql, "migrate_venue", $more);
 
 	exit();
 
