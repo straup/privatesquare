0cbe511155586adc3a2cab185b2f526a6b56e98a
straup <straup@gmail.com>
2013-11-26 08:41:31 -0500

    start work on geojson exports

--- www/include/lib_privatesquare_export_geojson.php
+++ www/include/lib_privatesquare_export_geojson.php
@@ -69,4 +69,73 @@
 
 	##############################################################################
 
+	# THIS HAS NOT BEEN TESTED YET (20131126/straup)
+
+	function privatesquare_export_geojson_row($row, $fh, $more=array()){
+
+		$defaults = array(
+			'index' => 0,
+			'send_headers' => 1
+		);
+
+		$more = array_merge($defaults, $more);
+
+		$massage_more = array(
+			'inflate_all' => 1,
+		);
+
+		privatesquare_export_massage_checkin($row, $massage_more);
+		ksort($row);
+
+		$lat = floatval($row['latitude']);
+		$lon = floatval($row['longitude']);
+
+		$feature = array(
+			'type' => 'Feature',
+			'id' => $row['id'],
+			'properties' => $row,
+			'geometry' => array(
+				'type' => 'Point',
+				'coordinates' => array($lon, $lat),
+			),
+		);
+
+		/*
+		$geojson = array(
+			'type' => 'FeatureCollection',
+			'features' => array(),
+		);
+		*/
+
+		if (($more['is_first']) && ($more['send_headers'])){
+
+			$map = privatesquare_export_valid_formats();
+
+			$headers = array(
+				'Content-type' => $map['geojson'],
+			);
+
+			privatesquare_export_send_headers($headers);
+
+			# SEND FeatureCollection crap here...
+		}
+
+		fwrite($fh, json_encode($feature));
+
+		if (! $more['is_last']){
+			fwrite($fh, ",");	# sad face...
+		}
+
+		else {
+			# SEND END OF FeatureCollection crap here...
+		}
+
+		if (isset($more['donot_send'])){
+			return okay();
+		}
+
+	}
+
+	##############################################################################
+
 	# the end
