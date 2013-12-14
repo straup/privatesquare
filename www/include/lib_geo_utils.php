d694238e375a0874f70744351cc453b44a007fc2
straup <straup@gmail.com>
2013-12-14 11:03:57 -0500

    stub out point-in-poly code

--- www/include/lib_geo_utils.php
+++ www/include/lib_geo_utils.php
@@ -135,10 +135,51 @@
 	# http://www.sidvind.com/wiki/Point-in-polygon:_Jordan_Curve_Theorem
 	# http://alienryderflex.com/polygon/
 	# http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
+	# http://assemblysys.com/php-point-in-polygon-algorithm/
 
-	function geo_utils_is_point_in_polygon(){
+	# $coords are assumed to be a array(array($lat, $lon) ... )
 
-		 # please write me...
+	function geo_utils_is_point_in_polygon($lat, $lon, &$coords){
+
+	# please write me...
+
+		 /*
+		$intersections = 0;
+		$count_coords = count($coords);
+ 
+		for ($i=1; $i < $count_coords; $i++){
+
+			$vertex1 = $coords[$i-1];
+			$vertex2 = $coords[$i];
+
+			# Check if point is on an horizontal polygon boundary
+
+			if ($vertex1['y'] == $vertex2['y'] &&
+			    $vertex1['y'] == $point['y'] &&
+			    $point['x'] > min($vertex1['x'], $vertex2['x']) &&
+			    $point['x'] < max($vertex1['x'], $vertex2['x'])){
+
+				return "boundary";
+			}
+
+
+			if ($point['y'] > min($vertex1['y'], $vertex2['y']) and $point['y'] <= max($vertex1['y'], $vertex2['y']) and $point['x'] <= max($vertex1['x'], $vertex2['x']) and $vertex1['y'] != $vertex2['y']) {
+                $xinters = ($point['y'] - $vertex1['y']) * ($vertex2['x'] - $vertex1['x']) / ($vertex2['y'] - $vertex1['y']) + $vertex1['x'];
+                if ($xinters == $point['x']) { // Check if point is on the polygon boundary (other than horizontal)
+                    return "boundary";
+                }
+                if ($vertex1['x'] == $vertex2['x'] || $point['x'] <= $xinters) {
+                    $intersections++;
+                }
+            }
+        }
+
+		# If the number of edges we passed through is odd
+		# then it's in the polygon.
+
+		return ($intersections % 2 != 0) ? "inside" : "outside";
+
+		*/
 	}
 
 	# the end
