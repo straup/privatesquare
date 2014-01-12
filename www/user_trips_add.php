cf411989571c637379d7837a5483ce7250842be7
straup <straup@gmail.com>
2014-01-12 13:13:08 -0500

    travel types

--- www/user_trips_add.php
+++ www/user_trips_add.php
@@ -6,9 +6,11 @@
 	login_ensure_loggedin();
 
 	$user = $GLOBALS['cfg']['user'];
-
 	$GLOBALS['smarty']->assign_by_ref("user", $user);
 	
+	$travel_map = trips_travel_type_map();
+	$GLOBALS['smarty']->assign_by_ref("travel_map", $travel_map);
+
 	$GLOBALS['smarty']->display("page_user_trips_add.txt");
 	exit();
 
