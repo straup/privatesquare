3d3823bc546929ed03d2172c2e187ba4f79e495f
straup <straup@gmail.com>
2014-08-04 00:22:06 +0200

    stub out signin by email stuff

--- www/signin_email.php
+++ www/signin_email.php
@@ -5,82 +5,47 @@
 
 	login_ensure_loggedout();
 
-
-	#
-	# pass through
-	#
-
 	$redir = request_str('redir');
-	$smarty->assign('redir', $redir);
-
-
-	#
-	# try and sign in?
-	#
+	$GLOBALS['smarty']->assign('redir', $redir);
 
 	if (post_str('signin')){
 
 		$email		= post_str('email');
 		$password	= post_str('password');
 
-		$smarty->assign('email', $email);
+		$GLOBALS['smarty']->assign('email', $email);
 
 		$ok = 1;
 
-
-		#
-		# required fields?
-		#
-
 		if ((!strlen($email)) || (!strlen($password))){
 
-			$smarty->assign('error_missing', 1);
+			$GLOBALS['smarty']->assign('error_missing', 1);
 			$ok = 0;
 		}
-
-
-		#
-		# user exists?
-		#
 
 		if ($ok){
 			$user = users_get_by_email($email);
 
 			if (!$user['id']){
 
-				$smarty->assign('error_nouser', 1);
+				$GLOBALS['smarty']->assign('error_nouser', 1);
 				$ok = 0;
 			}
 		}
 
-
-		#
-		# users deleted?
-		#
-
 		if ($ok && $user['deleted']){
 
-			$smarty->assign('error_deleted', 1);
+			$GLOBALS['smarty']->assign('error_deleted', 1);
 			$ok = 0;
 		}
-
-
-		#
-		# password match
-		#
 
 		if ($ok){
 
 			if (! passwords_validate_password_for_user($password, $user)){
-				$smarty->assign('error_password', 1);
+				$GLOBALS['smarty']->assign('error_password', 1);
 				$ok = 0;
 			}
 		}
-
-
-		#
-		# it's all good - sign in
-		#
 
 		if ($ok){
 			$redir = ($redir) ? $redir : '/';
@@ -90,9 +55,7 @@
 		}
 	}
 
+	$GLOBALS['smarty']->display('page_signin.txt');
+	exit();
 
-	#
-	# output
-	#
-
-	$smarty->display('page_signin.txt');
+?>
